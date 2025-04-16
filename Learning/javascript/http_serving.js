67ec81b3bcd6ee43e9d29bfe6a95a16d4e4e56e9
NIRMAL KUMAR R <firenirmal45@gmail.com>
2025-04-16 18:46:14 +0530

    16 apr

---
+++ Learning/javascript/http_serving.js
@@ -0,0 +1,42 @@
+function serve_http() {
+	var http = require("http")
+
+	var server = http.createServer(function(request, response) {
+
+		if (request.url == "/"){
+			response.write("request recieved")
+			response.end()
+			}
+
+		if (request.url == "/home") {
+			response.write("welcome to home page!")
+			response.end()
+			}	
+
+		if (request.url == "/files") {
+			var serveStatic = require("serve-static")
+			var finalHandler = require("finalhandler")
+
+			var serve = serveStatic("../ktt")
+			serve(request, response, finalHandler(request, response))
+		}
+
+		}
+	)
+
+	server.listen(8888)
+}
+
+
+var express = require("express")
+var app = express()
+
+app.get("/", function (request, response) {
+	response.send("Request Recieved")
+})
+
+app.get("/home", function (request, response) {
+	response.send("welcome to home page")
+})
+
+app.listen(8889)
